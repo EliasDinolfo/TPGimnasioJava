bc857bd7e2ee2656e9fa7a50711ccbecf875ee5c
Elias <eliasdinolfo99@gmail.com>
2022-08-23 16:09:09 -0300

    Clase plan añadida al paquete de entidades

---
+++ TPGimnasio/src/entities/Plan.java
@@ -0,0 +1,35 @@
+package entities;
+
+import java.time.LocalDate;
+
+public class Plan {
+	private int id_plan;
+	private String nombre;
+	private String descripcion;
+	private LocalDate fecha_expiracion;
+	
+	public int getId_plan() {
+		return id_plan;
+	}
+	public void setId_plan(int id_plan) {
+		this.id_plan = id_plan;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public LocalDate getFecha_expiracion() {
+		return fecha_expiracion;
+	}
+	public void setFecha_expiracion(LocalDate fecha_expiracion) {
+		this.fecha_expiracion = fecha_expiracion;
+	}
+}
