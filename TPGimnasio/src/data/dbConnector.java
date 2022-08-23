fa03594b80fac13d5d4d6a99b692c2e5d8cbc825
Elias <eliasdinolfo99@gmail.com>
2022-08-23 17:51:14 -0300

    Creacion de la clase dbConnector de la capa de data

--- TPGimnasio/src/data/dbConnector.java
+++ TPGimnasio/src/data/dbConnector.java
@@ -1,5 +1,54 @@
 package data;
+import java.sql.*;
 
 public class dbConnector {
-
+private static dbConnector instancia;
+	
+	private String driver="com.mysql.cj.jdbc.Driver";
+	private String host="localhost";
+	private String port="3306";
+	private String user="java";
+	private String password="himitsu";
+	private String db="java";
+	private int conectados=0;
+	private Connection conn=null;
+	
+	private dbConnector() {
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static dbConnector getInstancia() {
+		if (instancia == null) {
+			instancia = new dbConnector();
+		}
+		return instancia;
+	}
+	
+	public Connection getConn() {
+		try {
+			if(conn==null || conn.isClosed()) {
+				conn=DriverManager.getConnection("jdbc:mysql://"+host+":"+port+"/"+db, user, password);
+				conectados=0;
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		conectados++;
+		return conn;
+	}
+	
+	public void releaseConn() {
+		conectados--;
+		try {
+			if (conectados<=0) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 }
