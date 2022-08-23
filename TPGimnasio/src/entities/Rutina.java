b30ed6a083489d9a510b8205f3d85f3dcfd7b383
Mauro Jimenez <maurojim123@gmail.com>
2022-08-23 15:45:44 -0300

    Clase rutina añadida al paquete entities

---
+++ TPGimnasio/src/entities/Rutina.java
@@ -0,0 +1,43 @@
+package entities;
+
+public class Rutina {
+	private int id_rutina;
+	private String nombre;
+	private String semanas;
+	private String nivel;
+	private String comentario;
+	
+	public int getId_rutina() {
+		return id_rutina;
+	}
+	public void setId_rutina(int id_rutina) {
+		this.id_rutina = id_rutina;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getSemanas() {
+		return semanas;
+	}
+	public void setSemanas(String semanas) {
+		this.semanas = semanas;
+	}
+	public String getNivel() {
+		return nivel;
+	}
+	public void setNivel(String nivel) {
+		this.nivel = nivel;
+	}
+	public String getComentario() {
+		return comentario;
+	}
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
+	
+	
+	
+}
