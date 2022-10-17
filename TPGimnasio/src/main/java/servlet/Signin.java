63d228e56e892badfde5ce41cc6e09d8a28e0e4e
maurojjzz <maurojim123@gmail.com>
2022-10-16 21:14:44 -0300

    creacion paquete servlet y creacion servlet Signin

---
+++ TPGimnasio/src/main/java/servlet/Signin.java
@@ -0,0 +1,47 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Signin
+ */
+@WebServlet({ "/Signin", "/SIGNIN", "/signin", "/signIn", "/SignIn" })
+public class Signin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Signin() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("get at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		response.getWriter().append("User: ").append(username)
+		.append(" Password: ").append(password)
+		.append(" post at: ").append(request.getContextPath());
+		//doGet(request, response);
+	}
+
+}
